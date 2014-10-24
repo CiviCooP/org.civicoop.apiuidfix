555a65138ecaec2efa4f8a8f1e856e39e80a134e
Jaap Jansma <info@edeveloper.nl>
2014-10-24 20:07:14 +0200

    update

---
+++ CRM/Apiuidfix/ApiWrapper.php
@@ -0,0 +1,39 @@
+<?php
+
+class CRM_Apiuidfix_ApiWrapper implements API_Wrapper {
+  /**
+   * the wrapper contains a method that allows you to alter the parameters of the api request (including the action and the entity)
+   */
+  public function fromApiInput($apiRequest) {
+    $session = CRM_Core_Session::singleton();
+    if (!empty($session->get('userID'))) {
+      return $apiRequest;
+    }
+    
+    $valid_user = FALSE;
+
+    // Check and see if a valid secret API key is provided.
+    $api_key = CRM_Utils_Request::retrieve('api_key', 'String', $store, FALSE, NULL, 'REQUEST');
+    if (!$api_key || strtolower($api_key) == 'null') {
+      return $apiRequest;
+    }
+    $valid_user = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_Contact', $api_key, 'id', 'api_key');
+
+    // If we didn't find a valid user, die
+    if (empty($valid_user)) {
+      return $apiRequest;
+    }
+    
+    //now set the UID into the session
+    $session = CRM_Core_Session::singleton();
+    $session->set('userID', $valid_user);
+    return $apiRequest;
+  }
+  /**
+   * alter the result before returning it to the caller.
+   */
+  public function toApiOutput($apiRequest, $result) {
+    return $result;
+  }
+}
+
